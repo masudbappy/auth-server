72bc5b6cea5cc285431cdeb90c020abe2abc7fb4
masudbappy <masud.bappy90@gmail.com>
2025-08-16 18:10:13 +0600

    Added GlobalExceptionHandler

---
+++ src/main/java/com/tradeflow/auth_server/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.tradeflow.auth_server.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
