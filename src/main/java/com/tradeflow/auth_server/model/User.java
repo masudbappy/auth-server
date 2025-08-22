1892998a1ab799afb6c500c84972cddb405e3d52
masudbappy <masud.bappy90@gmail.com>
2025-08-22 09:57:31 +0600

    Updated User entity

--- src/main/java/com/tradeflow/auth_server/model/User.java
+++ src/main/java/com/tradeflow/auth_server/model/User.java
@@ -1,7 +1,6 @@
 package com.tradeflow.auth_server.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.security.core.GrantedAuthority;
